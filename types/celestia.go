dcc6d9e1516ed07364a3d7d8dbcec2a6f1cb9449
christopherbrumm <christopherbrumm2000@gmail.com>
2024-05-27 16:06:11 +0200

    update: CelestiaValue type

--- types/celestia.go
+++ types/celestia.go
@@ -1,14 +1,12 @@
 package types
 
-import (
-	"encoding/json"
-)
+import "encoding/json"
 
 type CelestiaValue struct {
 	SharesByNamespace []NamespacedShares `json:"sharesByNamespace"`
 }
 
 type NamespacedShares struct {
-	Data        []json.RawMessage `json:"data"`
-	NamespaceId string            `json:"namespace_id"`
+	Data        json.RawMessage `json:"data"`
+	NamespaceId string          `json:"namespace_id"`
 }
